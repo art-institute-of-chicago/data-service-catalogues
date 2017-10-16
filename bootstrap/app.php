2ae79912376553e61335017101fde3e38e166014
Illya Moskvin <imoskvin@artic.edu>
2017-10-16 13:17:20 -0500

    Remove user and auth stuff

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -63,10 +63,6 @@ $app->singleton(
 //    App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
-
 /*
 |--------------------------------------------------------------------------
 | Register Service Providers
@@ -79,7 +75,6 @@ $app->singleton(
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
