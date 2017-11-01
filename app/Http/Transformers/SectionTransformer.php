72c3a36232ce99aa952ac1cd4cd933df5fda05dc
Illya Moskvin <imoskvin@artic.edu>
2017-11-01 10:41:40 -0500

    Use Eloquent to retrieve related ids
    This is safer than using the column value. If the column refers to an
    entity that doesn't exist, our API will return null instead. We do this
    in the aggregator core to enable faster imports. We should use the `??`
    syntax there as well, since it's less verbose.

--- app/Http/Transformers/SectionTransformer.php
+++ app/Http/Transformers/SectionTransformer.php
@@ -16,9 +16,9 @@ class SectionTransformer extends TransformerAbstract
             'title' => $section->title,
             'revision' => $section->revision,
             'source_id' => $section->source_id,
-            'publication_id' => $section->publication_id,
-            'parent_id' => $section->parent_id,
-            'child_ids' => $section->children()->pluck('id'),
+            'publication_id' => $section->publication->id ?? null,
+            'parent_id' => $section->parent->id ?? null,
+            'child_ids' => $section->children->pluck('id'),
         ];
 
     }
