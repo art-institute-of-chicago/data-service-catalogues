3716d5a146ab3afaf5371659120d1ced5f712865
nikhiltri <ntrivedi@artic.edu>
2018-03-27 14:55:27 -0500

    Schedule import command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+
+        $schedule->command('import:pubs --quiet')
+            ->dailyAt('23:' .(config('app.env') == 'production' ? '00' : '15'))
+            ->withoutOverlapping()
+            ->appendOutputTo(storage_path('logs/import.log'))
+            ->sendOutputTo(storage_path('logs/import-last-run.log'))
+            ->emailOutputTo([env('LOG_EMAIL_1'), env('LOG_EMAIL_2')], true);
+
     }
 }
