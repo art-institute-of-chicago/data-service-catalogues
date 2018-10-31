e79917f274ec293c4a9f41f9b5e90b4999e9df6e
Illya Moskvin <imoskvin@artic.edu>
2018-10-31 13:26:31 -0500

    Fix settings in config/app.php
    https://github.com/art-institute-of-chicago/data-hub-foundation/commit/09adb33
    https://github.com/art-institute-of-chicago/data-hub-foundation/commit/a89a276

--- config/app.php
+++ config/app.php
@@ -109,4 +109,43 @@ return [
 
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Application Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default timezone for your application, which
+    | will be used by the PHP date and date-time functions. We have gone
+    | ahead and set this to a sensible default for you out of the box.
+    |
+    */
+
+    'timezone' => 'America/Chicago',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services your application utilizes. Set this in your ".env" file.
+    |
+    */
+
+    'env' => env('APP_ENV', 'production'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Application URL
+    |--------------------------------------------------------------------------
+    |
+    | This URL is used by the console to properly generate URLs when using
+    | the Artisan command line tool. You should set this to the root of
+    | your application so that it is used when running Artisan tasks.
+    |
+    */
+
+    'url' => env('APP_URL', 'http://localhost'),
+
 ];
